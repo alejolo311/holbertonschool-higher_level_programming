960c6c3d32d3cf33fff9436093c2c37aff75f6ca
alejolo311 <1147@holbertonschool.com>
2019-12-10 12:35:39 -0500

    print comb6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-listint_t *insert_node(listint_t **head, int number);
+for i in range(10):
+    for j in range(10):
+        if j == 9 and i == 8:
+            print("{}{}".format(i, j))
+        else:
+            if j > i and i is not 8:
+                print("{}{}".format(i, j), end=", ")

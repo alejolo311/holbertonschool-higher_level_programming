133c1caf70390fc56801208f843215e4e19d4f11
alejolo311 <1147@holbertonschool.com>
2019-12-10 12:22:03 -0500

    implement a new way

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-alphabet = ""
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) == 'q' or chr(i) == 'e':
-        pass
-    else:
-        alphabet += chr(i)
-print("{}".format(alphabet, end=''))
+print("".format()
+        .join([chr(i) for i in range(ord('a'), ord('z') + 1)
+               if chr(i) not in ['q', 'e']]), end="")

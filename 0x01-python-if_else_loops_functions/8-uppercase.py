c256f7d67f10112f20c060bbd1f4595fcfa2511e
alejolo311 <1147@holbertonschool.com>
2019-12-10 13:00:04 -0500

    implement a new way

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    result = ''
-    for char in str:
-        if ord(char) >= 65:
-            result += chr(ord(char) - 32)
-    print(result)
+    string = ""
+    for i in range(len(str)):
+        if ord(str[i]) < ord('z') + 1 and ord(str[i]) > ord('a') - 1:
+            string += chr(ord(str[i]) - 32)
+        else:
+            string += chr(ord(str[i]))
+    print("{:s}".format(string))

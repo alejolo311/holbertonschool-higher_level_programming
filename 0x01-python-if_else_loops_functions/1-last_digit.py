9cff97c88acf9928409eadcd979cf4e72db10e8a
Alejo López <1147@holbertonschool.com>
2019-12-10 15:47:12 +0000

    adding the task 0 and 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
-def uppercase(str):
+import random
+number = random.randint(-10000, 10000)
+if number > 0:
+    last = number % 10
+else:
+    last = abs(number) % 10 * -1
+
+print("Last digit of {} is {}".format(number, last), end="")
+if last > 5:
+    print(" and is greater than 5")
+elif last == 0:
+    print(" and is 0")
+else:
+    print(" and is less than 6 and not 0")

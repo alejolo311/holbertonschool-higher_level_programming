27a35acf78f7b4139636d9ac3545df8ffca0f263
alejolo311 <1147@holbertonschool.com>
2019-12-10 11:53:27 -0500

    adding task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-def print_last_digit(number):
+
+letter = 'a'
+alphabet = ""
+for i in range(0, 26):
+    alphabet += letter
+    letter = chr(ord(letter) + 1)
+print("{}".format(alphabet, end=''))

f385b0d0f2959b666819d2a146bbd67ff337e9b8
alejolo311 <1147@holbertonschool.com>
2019-12-12 13:29:41 -0500

    fix a typo

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 if __name__ == '__main__':
     import sys
     if len(sys.argv) == 1:
-        print("{:d} argument.".format(num_arg - 1))
+        print("{} argument.".format(num_arg - 1))
     elif len(sys.argv) == 2:
-        print("{:d} argument:"format(len(sys.argv) - 1))
+        print("{} argument:".format(len(sys.argv) - 1))
     else:
-        print("{:d} arguments:".format(len(sys.argv) - 1))
+        print("{} arguments:".format(len(sys.argv) - 1))
     for i, e in enumerate(sys.argv):
         if i > 0:
             print("{}: {}".format(i, e))

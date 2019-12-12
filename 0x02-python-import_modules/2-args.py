7f6e48709ff0fcfd57d3fa049127f616a9e5fc52
alejolo311 <1147@holbertonschool.com>
2019-12-12 13:17:31 -0500

    fix something

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-import sys
-
-if len(sys.argv) is 1:
-    print("0 arguments.")
-else:
-    print("{} arguments:".format(len(sys.argv) - 1))
-for i, e in enumerate(sys.argv):
-    if i > 0:
-        print("{}: {}".format(i, e))
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) is 1:
+        print("0 arguments.")
+    elif len(sys.argv) > 3:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    else:
+        print("{} argument:".format(len(sys.argv) - 1))
+    for i, e in enumerate(sys.argv):
+        if i > 0:
+            print("{}: {}".format(i, e))

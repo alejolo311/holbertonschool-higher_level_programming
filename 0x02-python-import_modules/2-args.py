5fa624a17fb19dd2796a036c3445e169c8dfe9a0
alejolo311 <1147@holbertonschool.com>
2019-12-12 13:24:10 -0500

    fix the 0 case

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,11 @@
 if __name__ == '__main__':
     import sys
     if len(sys.argv) is 1:
-        print("0 arguments.")
-    elif len(sys.argv) > 3:
-        print("{:d} arguments:".format(len(sys.argv) - 1))
+        print("0 argument.")
     elif len(sys.argv) is 2:
         print("{} argument.".format(len(sys.argv) - 1))
     else:
-        print("{} argument:".format(len(sys.argv) - 1))
+        print("{} arguments:".format(len(sys.argv) - 1))
     for i, e in enumerate(sys.argv):
         if i > 0:
             print("{}: {}".format(i, e))

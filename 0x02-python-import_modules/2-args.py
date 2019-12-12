ada496440d32bbe53515888137944ed1e88f5819
alejolo311 <1147@holbertonschool.com>
2019-12-12 13:20:02 -0500

    fix something

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,9 @@ if __name__ == '__main__':
     if len(sys.argv) is 1:
         print("0 arguments.")
     elif len(sys.argv) > 3:
-        print("{} arguments:".format(len(sys.argv) - 1))
+        print("{:d} arguments:".format(len(sys.argv) - 1))
+    elif len(sys.argv) is 2:
+        print("{} argument.".format(len(sys.argv) - 1))
     else:
         print("{} argument:".format(len(sys.argv) - 1))
     for i, e in enumerate(sys.argv):

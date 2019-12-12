4cd94b5b9093376e64838088ab414d6b15634cc6
alejolo311 <1147@holbertonschool.com>
2019-12-12 13:40:33 -0500

    fix the F#$% s

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
     if len(sys.argv) == 1:
-        print("{} argument.".format(len(sys.argv) - 1))
+        print("{} arguments.".format(len(sys.argv) - 1))
     elif len(sys.argv) == 2:
         print("{} argument:".format(len(sys.argv) - 1))
     else:

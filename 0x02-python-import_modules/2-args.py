ccba3e22625e8baa09d673c0a85e280240033c66
alejolo311 <1147@holbertonschool.com>
2019-12-12 10:34:49 -0500

    count args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+import sys
+
+if len(sys.argv) is 1:
+    print("0 arguments.")
+else:
+    print("{} arguments:".format(len(sys.argv) - 1))
+for i, e in enumerate(sys.argv):
+    if i > 0:
+        print("{}: {}".format(i, e))

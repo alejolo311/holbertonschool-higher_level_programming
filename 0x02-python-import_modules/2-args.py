eabff7854ea7b7379543b0cda44563c7fe037abd
alejolo311 <1147@holbertonschool.com>
2019-12-12 13:37:35 -0500

    change the way to print

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
         print("{} argument:".format(len(sys.argv) - 1))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
-    for i, e in enumerate(sys.argv):
-        if i > 0:
-            print("{}: {}".format(i, e))
+    for i in range(len(sys.argv)):
+        if i == 0:
+            continue
+        print("{:d}: {:s}".format(i, sys.argv[i]))

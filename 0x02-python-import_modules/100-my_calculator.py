256a3f92cf53599979274d215fce3c295f1c3e2e
Alejo López <alejolo311@gmail.com>
2019-12-12 20:43:12 -0500

    fix the if

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     from calculator_1 import add, sub, mul, div
-    import syss
+    import sys
     args = sys.argv
     arg_len = len(sys.argv) - 1
     operators = {"+": add, "-": sub, "*": mul, "/": div}
     if arg_len > 3 or arg_len < 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    if args[2] not in operators:
+    if args[2] not in "+-*/":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     a = int(args[1])

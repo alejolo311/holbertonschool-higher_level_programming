d4c2178eaf0d1244acabc1432b87c062b2863309
Alejo lopez <alejolo311@gmail.com>
2020-01-21 20:54:12 +0000

    load from

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
+
+"""
+module for load_from_json_file.
+"""
+
+import json
+
+
 def load_from_json_file(filename):
+    """Create an Object"""
+    with open(filename, 'r') as f:
+        return json.load(f)

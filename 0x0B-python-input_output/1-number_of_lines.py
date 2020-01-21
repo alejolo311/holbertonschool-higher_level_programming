ff6d0cc037b233e364814a81f19c3711512b9561
Alejo lopez <alejolo311@gmail.com>
2020-01-21 18:57:55 +0000

    lines

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+
+"""
+module for number_of_lines.
+"""
+
+
 def number_of_lines(filename=""):
+    """Return the number of lines"""
+    i = 0
+    with open(filename, 'r') as f:
+        for line in f:
+            i = i + 1
+    return i

493d4c5258e24cdfc8ecfdf6aac7cf2d1e0c292d
Alejo lopez <alejolo311@gmail.com>
2020-01-21 22:30:20 +0000

    search and replace

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+
+"""
+module for append_after.
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Insert after a string"""
+    with open(filename, 'r') as f:
+        data = f.readlines()
+
+    out = ""
+    for line in data:
+        out = out + line
+        if search_string in line:
+            out = out + new_string
+
+    with open(filename, 'w') as f:
+        f.write(out)

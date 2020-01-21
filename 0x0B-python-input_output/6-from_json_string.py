ce3faa607abdd680e992783d8c8a3fc38ec40780
Alejo lopez <alejolo311@gmail.com>
2020-01-21 20:50:40 +0000

    task 6 and clena

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+
+"""
+module for from_json_string.
+"""
+
+import json
+
+
 def from_json_string(my_str):
+    """Return an object (Python data structure)"""
+    return json.loads(my_str)

f2d23b2d767bcd42be45e5fdff5e45c5a960f0d2
Alejo lopez <alejolo311@gmail.com>
2020-01-21 20:25:50 +0000

    whats numbers

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
+
+"""
+module for read_lines.
+"""
+
+
 def read_lines(filename="", nb_lines=0):
+    """Read n lines of a text file"""
+    i = len(open(filename).readlines())
+    with open(filename, 'r') as f:
+        if nb_lines > 0 and nb_lines < i:
+            while nb_lines:
+                print(f.readline(), end="")
+                nb_lines -= 1
+        else:
+            read_data = f.read()
+            print(read_data, end="")

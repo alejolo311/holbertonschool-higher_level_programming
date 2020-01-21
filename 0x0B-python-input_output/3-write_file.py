ea247dd6cf2dac8f6d6ec5232e8a6ee69e6fb67a
Alejo lopez <alejolo311@gmail.com>
2020-01-21 20:44:07 +0000

    writw

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+
+"""
+module for write_file
+"""
+
+
 def write_file(filename="", text=""):
+    """Write a string to a text file"""
+    with open(filename, 'w') as f:
+        c = f.write(text)
+    return c

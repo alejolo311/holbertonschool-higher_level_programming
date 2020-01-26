7238d835089925395793a95bb815896ba6695c91
alejolo311 <alejolo311@gmail.com>
2020-01-26 16:38:12 +0000

    creating the base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+"""
+module for Base class
+"""
+
+
+class Base:
+    """A base"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Intialize a base."""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

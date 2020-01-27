68722c91a33ca4ee9bc05e94c67ecada990bb25d
alejolo311 <alejolo311@gmail.com>
2020-01-27 18:44:51 +0000

    to save ina file jeje

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,3 +30,19 @@ class Base:
         if any(type(i) != dict for i in list_dictionaries):
             raise TypeError("list_dictionaries must contain dictionaries")
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, tosave):
+        """Write"""
+        if type(tosave) != list and tosave is not None:
+            raise TypeError("list_objs must be a list")
+        if tosave is None or tosave == []:
+            out = []
+        else:
+            first = type(tosave[0])
+            if any(type(i) != first for i in tosave):
+                raise ValueError("all elements of list_objs must match")
+            out = [i.to_dictionary() for i in tosave]
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as f:
+            f.write(cls.to_json_string(out))

80121ef86da6c3a4723d28549de630cff49bf02d
alejolo311 <alejolo311@gmail.com>
2020-01-27 17:53:39 +0000

    a new static method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,8 @@
 module for Base class
 """
 
+import json
+
 
 class Base:
     """A base"""
@@ -17,3 +19,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """JSON string"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        if type(list_dictionaries) != list:
+            raise TypeError("list_dictionaries must be a list")
+        if any(type(i) != dict for i in list_dictionaries):
+            raise TypeError("list_dictionaries must contain dictionaries")
+        return json.dumps(list_dictionaries)

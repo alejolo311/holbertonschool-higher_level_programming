4961f1ce7ed662d7391b669fd5972094a8dfaeef
alejolo311 <alejolo311@gmail.com>
2020-01-28 02:47:34 +0000

    test

--- 0x0C-python-almost_a_circle/tests/tests_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/tests_models/test_base.py
@@ -14,11 +14,49 @@ class TestBase(unittest.TestCase):
     def setUp(self):
         Base._Base__nb_objects = 0
 
-    def test_0(self):
+    def test_00(self):
         """Test number 0 for base"""
         base0 = Base()
         base1 = Base()
-        base2 = Base()
         self.assertEqual(base0.id, 1)
         self.assertEqual(base1.id, 2)
-        self.assertEqual(base2.id, 3)
+
+    def test_01(self):
+        """Test number 1 for base"""
+        base0 = Base(0)
+        self.assertEqual(base0.id, 0)
+
+    def test_02(self):
+        """Test number 2 for base"""
+        base0 = Base(12)
+        self.assertEqual(base0.id, 12)
+
+    def test_03(self):
+        """Test number 3 for base"""
+        base0 = Base("test")
+        self.assertEqual(base0.id, "test")
+
+    def test_04(self):
+        """Test number 4 for base"""
+        base0 = Base(-12)
+        self.assertEqual(base0.id, -12)
+
+    def test_05(self):
+        """Test number 5 for base."""
+        base0 = Base(None)
+        self.assertEqual(base0.id, 1)
+
+    def test_06(self):
+        """Test number 6 for base"""
+        base0 = Base({"test": "test"})
+        self.assertEqual(base0.id, {"test": "test"})
+
+    def test_07(self):
+        """Test number 7 for base"""
+        base0 = Base([1, 2, 3])
+        self.assertEqual(base0.id, [1, 2, 3])
+
+    def test_08(self):
+        """Test number 8 for base"""
+        base0 = Base(12.3)
+        self.assertEqual(base0.id, 12.3)

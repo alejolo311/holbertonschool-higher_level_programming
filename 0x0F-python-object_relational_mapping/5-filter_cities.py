35da4584ffcc4594a733583b07879aa36bbf921c
Alejo Lopez <alejolo311@gmail.com>
2020-03-29 09:35:20 -0500

    re-write all

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -17,13 +17,11 @@ if __name__ == '__main__':
                          db=DB_Name,
                          charset="utf8")
     cursor = DB.cursor()
-    query = """SELECT cities.name
-                 FROM cities, states
-                 WHERE BINARY states.name = %s
-                 AND cities.state_id = states.id
-                 ORDER BY cities.id ASC"""
+    query = "SELECT cities.name FROM cities " +\
+            "JOIN states ON cities.state_id = states.id " +\
+            "WHERE states.name = %s ORDER BY cities.id ASC"
     cursor.execute(query, (state_name,))
     rows = cursor.fetchall()
     print(", ".join([row[0] for row in rows]))
-    cur.close()
-    db.close()
+    cursor.close()
+    DB.close()

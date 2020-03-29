a81dc34e07219e39f783b40deaba39ee862b7f67
Alejo Lopez <alejolo311@gmail.com>
2020-03-29 09:10:33 -0500

    fetch cities by state

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""cities by state"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    DB_Name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    DB = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=DB_Name,
+                         charset="utf8")
+    cursor = DB.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name\
+                FROM cities LEFT JOIN states\
+                ON states.id = cities.state_id\
+                ORDER BY cities.id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)

a4c39b5462b137fa5ae4825d3fc6e0e3af3f3ef9
Alejo Lopez <alejolo311@gmail.com>
2020-03-29 12:05:52 -0500

    im so stupid

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     DB_name = sys.argv[3]
+    state_name = sys.argv[4]
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(username, password, DB_name),

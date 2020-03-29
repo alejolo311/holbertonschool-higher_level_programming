4230b1b5bf83f89d9816453ee5034db87aeab463
Alejo Lopez <alejolo311@gmail.com>
2020-03-29 08:58:56 -0500

    filter the states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""lists all states whose first letter is N"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    DB_Name = sys.argv[3]
+
+    DB = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=DB_Name,
+                         charset="utf8")
+    cursor = DB.cursor()
+    cursor.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id ASC""")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)

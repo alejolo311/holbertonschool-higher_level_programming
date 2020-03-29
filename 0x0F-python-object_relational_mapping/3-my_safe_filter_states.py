beffde536970748614be6e0d418581d44fdfa2f0
Alejo Lopez <alejolo311@gmail.com>
2020-03-29 09:07:52 -0500

    avoid SQL inyection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""takes args and display the matches"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    DB_Name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    DB = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=DB_Name,
+                         charset="utf8")
+    cursor = DB.cursor()
+    cur.execute("SELECT * FROM states WHERE name=%s\
+                ORDER BY states.id ASC", (state_name,))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)

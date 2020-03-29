7217acd5cadc9c0df28b725ff5e639d413caf287
Alejo Lopez <alejolo311@gmail.com>
2020-03-29 09:12:54 -0500

    change the way in thats made the query

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,7 +8,6 @@ if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     DB_Name = sys.argv[3]
-    state_name = sys.argv[4]
 
     DB = MySQLdb.connect(host="localhost",
                          port=3306,
@@ -16,11 +15,9 @@ if __name__ == '__main__':
                          passwd=password,
                          db=DB_Name,
                          charset="utf8")
-    cursor = DB.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name\
-                FROM cities LEFT JOIN states\
-                ON states.id = cities.state_id\
-                ORDER BY cities.id ASC")
-    rows = cursor.fetchall()
+    curs = DB.cursor()
+    curs.execute("SELECT cities.id, cities.name, states.name FROM cities " +
+                 "JOIN states ON cities.state_id = states.id ORDER BY id ASC;")
+    rows = curs.fetchall()
     for row in rows:
         print(row)

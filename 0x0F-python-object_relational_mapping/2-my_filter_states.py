3783e2f041a9be5d80945e1a5ff7f21d68308f18
Alejo Lopez <alejolo311@gmail.com>
2020-03-29 09:02:33 -0500

    display the matches

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""takes args and display the matches"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    DB_Name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    DB = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=DB_Name,
+                         charset="utf8")
+    cursor = DB.cursor()
+    cursor.execute("SELECT * FROM states WHERE BINARY " +
+                   "name='{}' ".format(state_name) +
+                   "ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)

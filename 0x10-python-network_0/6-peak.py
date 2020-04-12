2443ce99c827d0192682a4389936c4bae054ff42
alejolo311 <alejolo311@gmail.com>
2020-04-12 12:14:01 -0500

    fix style

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,19 +4,21 @@ finds a peak in a list.
 O(log(n)) solution
 """
 
+
 def find_peak(list_of_integers):
     """Find peak"""
     if not list_of_integers:
         return None
     list_len = len(list_of_integers)
-    result = helper(list_of_integers, 0, len(list_of_integers) - 1,len(list_of_integers))
+    result = helper(list_of_integers, 0, len(list_of_integers)
+                    - 1, len(list_of_integers))
     return result
 
 
 def helper(_list, lowest, highest, n):
     """Helper func"""
-    mid = lowest + (highest - lowest)/2 
-    mid = int(mid) 
+    mid = lowest + (highest - lowest)/2
+    mid = int(mid)
     if (mid == 0 or _list[mid - 1] <= _list[mid]) and\
        (mid == n - 1 or _list[mid + 1] <= _list[mid]):
         return _list[mid]

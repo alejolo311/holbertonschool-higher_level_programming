767403734368bc5c32d7f1200d2d673a5cfb4662
alejolo311 <alejolo311@gmail.com>
2020-04-12 12:50:20 -0500

    print the status code, do a post json

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+# request and return the status code
+curl -s -L -I "$1" -o /dev/null -w '%{http_code}'
--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+# Sends a JSON POST
+curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"

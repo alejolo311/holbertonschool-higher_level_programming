71d9c1ff23e569ee1555b33e0257e82e6663f87f
alejolo311 <alejolo311@gmail.com>
2020-04-12 12:07:34 -0500

    do a post with paramateres, solve a python problem, fin the peak in a list

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+# POST AND SEND DATA
+curl -s -X POST -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD" "$1"
---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+finds a peak in a list.
+O(log(n)) solution
+"""
+
+def find_peak(list_of_integers):
+    """Find peak"""
+    if not list_of_integers:
+        return None
+    list_len = len(list_of_integers)
+    result = helper(list_of_integers, 0, len(list_of_integers) - 1,len(list_of_integers))
+    return result
+
+
+def helper(_list, lowest, highest, n):
+    """Helper func"""
+    mid = lowest + (highest - lowest)/2 
+    mid = int(mid) 
+    if (mid == 0 or _list[mid - 1] <= _list[mid]) and\
+       (mid == n - 1 or _list[mid + 1] <= _list[mid]):
+        return _list[mid]
+    elif (mid > 0 and _list[mid - 1] > _list[mid]):
+        return helper(_list, lowest, mid - 1, n)
+    else:
+        return helper(_list, mid + 1, highest, n)

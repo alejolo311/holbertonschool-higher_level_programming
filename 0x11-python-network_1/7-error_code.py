0723a136301eb80c77e85ecf15fe497ccb5c9609
alejolo311 <alejolo311@gmail.com>
2020-04-16 18:25:18 -0500

    fix variable name

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,7 +7,7 @@ from sys import argv
 if __name__ == "__main__":
     response = requests.get(argv[1])
     statusCode = response.status_code
-    if status >= 400:
+    if statusCode >= 400:
         print("Error code: " + str(statusCode))
     else:
         print(response.text)

2c98247bfae647d278fd69c9b18809597849f5b9
alejolo311 <alejolo311@gmail.com>
2020-04-16 18:21:59 -0500

    fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""send an email as a request"""
+"""send an email as a post request"""
 
 import requests
-from sys import arg
+from sys import argv
 
 if __name__ == "__main__":
     response = requests.post(argv[1], data={'email': argv[2]})

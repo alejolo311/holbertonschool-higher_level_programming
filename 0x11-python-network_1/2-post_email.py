d6bfa945bdf652e4c67ba53880706b5f74ad719b
alejolo311 <alejolo311@gmail.com>
2020-04-16 18:00:30 -0500

    task 2 3 and 4

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""send a email as a post"""
+
+import urllib.request
+from sys import argv
+
+if __name__ == "__main__":
+    body = urllib.parse.urlencode({'email': argv[2]})
+    body = body.encode('ascii')
+    with urllib.request.urlopen(argv[1], body) as url:
+        print(url.read().decode('utf-8'))
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""display the body"""
+
+import urllib.request
+from sys import argv
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(argv[1]) as url:
+            print(url.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        error_code = str(e).split(' ')[2][:-1]
+        print("Error code: " + str(error_code))

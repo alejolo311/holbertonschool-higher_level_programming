712e3abaa5ab5c7be086c2edd2d215d8bef89911
alejolo311 <alejolo311@gmail.com>
2020-04-16 17:46:08 -0500

    task 0 and task 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""intanet status"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as status:
+        read = status.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(read)))
+        print("\t- content: {}".format(read))
+        print("\t- utf8 content: {}".format(read.decode('utf-8')))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""show the request id"""
+
+import urllib.request
+from sys import argv
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(argv[1]) as request:
+        print(request.headers.get('X-Request-Id'))

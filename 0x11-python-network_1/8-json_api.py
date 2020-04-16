eb96fa53fd2c34d6bab1f62ff38ce929c6a53146
alejolo311 <alejolo311@gmail.com>
2020-04-16 18:31:42 -0500

    make a post with a json and wait for a json response

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+Takes in a letter and send a post
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    json = {'q': ""}
+    if len(argv) > 1:
+        json['q'] = argv[1]
+    response = requests.post("http://0.0.0.0:5000/search_user", json)
+    if "json" not in response.headers.get('content-type'):
+        print("Not a valid JSON")
+    else:
+        if response.json():
+            print("[{}] {}".format(response.json().get('id'),
+                  response.json().get('name')))
+        else:
+            print("No result")

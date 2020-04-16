c187d0603d502fd3375b8eceb77579f3812170e7
alejolo311 <alejolo311@gmail.com>
2020-04-16 18:17:44 -0500

    task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""show the id request"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.get(argv[1])
+    print(response.headers.get('x-request-id'))

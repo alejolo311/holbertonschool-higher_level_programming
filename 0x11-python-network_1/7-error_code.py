f87b19988f596dedc4ff685c640d3550ac43eca8
alejolo311 <alejolo311@gmail.com>
2020-04-16 18:23:50 -0500

    read the error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""display the body"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.get(argv[1])
+    statusCode = response.status_code
+    if status >= 400:
+        print("Error code: " + str(statusCode))
+    else:
+        print(response.text)

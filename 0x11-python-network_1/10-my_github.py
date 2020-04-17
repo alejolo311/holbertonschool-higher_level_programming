3164d5eca06f89c162842022858832f6092c4141
alejolo311 <alejolo311@gmail.com>
2020-04-16 20:11:08 -0500

    got to github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Auth at Github"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.get('https://api.github.com/user', auth=(argv[1],
+                            argv[2]))
+    if "json" not in response.headers.get('content-type'):
+        print("Not a valid JSON")
+    else:
+        response = response.json()
+        print(response.get('id'))

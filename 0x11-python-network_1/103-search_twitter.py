0142ccdb3fb4ab05b22f302e4711f875cf2fd586
alejolo311 <alejolo311@gmail.com>
2020-04-16 20:26:08 -0500

    tweet

--- 0x11-python-network_1/103-search_twitter.py
+++ 0x11-python-network_1/103-search_twitter.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python3
+"""Takes in three strings and sends a Tweet search to the Twitter API"""
+
+import requests
+import sys
+import base64
+
+if __name__ == "__main__":
+
+    key = '{}:{}'.format(sys.argv[1], sys.argv[2]).encode('ascii')
+    encoded_key = base64.b64encode(key)
+    encoded_key = encoded_key.decode('ascii')
+    response = requests.post('https://api.twitter.com/oauth2/token',
+                             headers={
+                                'Authorization': 'Basic {}'
+                                .format(encoded_key),
+                                'Content-Type':
+                                'application/x-www-form-urlencoded;charset=\
+                                    UTF-8'
+                             },
+                             data={
+                                'grant_type': 'client_credentials'
+                             })
+    token = response.json()['access_token']
+    response = requests.get('https://api.twitter.com/1.1/search/tweets.json',
+                            headers={
+                                'Authorization': 'Bearer {}'.format(token)
+                            },
+                            params={
+                                'q': sys.argv[3],
+                                'result_type': 'recent',
+                                'count': 5
+                            })
+    tweets = response.json()
+    for tweet in tweet_data['statuses']:
+        print("[{}] {} by {}".format(tweet.get('id_str'),
+                                     tweet.get('text'),
+                                     tweet.get('user').get('name')))

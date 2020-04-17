20393a466d1b60db2155e21bc03dc3406ea82a94
alejolo311 <alejolo311@gmail.com>
2020-04-16 20:14:25 -0500

    fetch commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Holberton interview"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    i = 0
+    commits = 'https://api.github.com/repos/'\
+              + argv[2] + '/' + argv[1] + '/commits'
+    response = requests.get(commits)
+    if "json" not in response.headers.get('content-type'):
+        print("Not a valid JSON")
+    else:
+        response = response.json()
+        for commit in response:
+            if i > 9:
+                break
+            print(commit.get('sha') + ': ', end="")
+            print(commit.get('commit').get('author').get('name'))
+            i += 1

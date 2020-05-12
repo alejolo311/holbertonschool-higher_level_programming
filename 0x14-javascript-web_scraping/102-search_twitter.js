762abc9ec786a6491397a632e8fd01c742d1cef9
alejolo311 <alejolo311@gmail.com>
2020-05-12 18:00:00 -0500

    errors

--- 0x14-javascript-web_scraping/102-search_twitter.js
+++ 0x14-javascript-web_scraping/102-search_twitter.js
@@ -3,20 +3,20 @@ const request = require('request');
 
 const auth = 'Basic ' + Buffer.from(process.argv[2] + ':' + process.argv[3]).toString('base64');
 
-request.post(
-  {
-    url: 'https://api.twitter.com/oauth2/token',
-    headers: {
-      Authorization: auth,
-      'content-type': 'application/x-www-form-urlencoded'
-    },
-    body: 'grant_type=client_credentials'
+let data = {
+  url: 'https://api.twitter.com/oauth2/token',
+  headers: {
+    Authorization: auth,
+    'content-type': 'application/x-www-form-urlencoded'
   },
-  (err, res, body) => {
-    if (err) {
-      console.log(err);
-    }
-    request.get({
+  body: 'grant_type=client_credentials'
+};
+
+request.post(data, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    data = {
       url: 'https://api.twitter.com/1.1/search/tweets.json',
       headers: {
         Authorization: `Bearer ${JSON.parse(body).access_token}`
@@ -26,14 +26,17 @@ request.post(
         result_type: 'recent',
         count: 5
       }
-    }, (err, res, body) => {
+    };
+    request.get(data, (err, res, body) => {
       if (err) {
         console.log(err);
-      }
-      const tweets = JSON.parse(body);
-      for (const tweet of tweets.statuses) {
-        console.log(`[${tweet.id}] ${tweet.text} by ${tweet.user.name}`);
+      } else {
+        const tweets = JSON.parse(body);
+        for (const tweet of tweets.statuses) {
+          console.log(`[${tweet.id}] ${tweet.text} by ${tweet.user.name}`);
+        }
       }
     });
   }
+}
 );

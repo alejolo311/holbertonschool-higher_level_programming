30816be613cfd81413d78229bf414375c9b51434
alejolo311 <alejolo311@gmail.com>
2020-05-12 16:09:33 -0500

    in order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(`http://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const characters = JSON.parse(body).characters;
+    order(characters, 0);
+  }
+});
+
+function order (characters, i) {
+  if (i === characters.length) { return; }
+  request(characters[i], (err, res, body) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(JSON.parse(body).name);
+      order(characters, i + 1);
+    }
+  });
+}

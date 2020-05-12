1981a4a895528788c97492306f6b2ba38dc288ba
alejolo311 <alejolo311@gmail.com>
2020-05-12 10:41:54 -0500

    print by char

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const _square = require('./5-square');
+
+class Square extends _square {
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.height));
+    }
+  }
+}
+
+module.exports = Square;

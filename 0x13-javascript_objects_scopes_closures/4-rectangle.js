979400f6b92d8da96a13d8bddb2247c976b5ac42
alejolo311 <alejolo311@gmail.com>
2020-05-12 10:31:34 -0500

    fix semistandar

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -14,12 +14,12 @@ class Rectangle {
       console.log('X'.repeat(this.width));
     }
   }
-  
-  rotate() {
-    [this.width, this.height] = [this.height, this.width]
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
   }
 
-  double() {
+  double () {
     this.width *= 2;
     this.height *= 2;
   }
